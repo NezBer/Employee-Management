92f3aa448d79681fed474aadfcc6a9296bcad281
NezBer <u650406401758@ms.kbu.ac.th>
2026-08-06 15:04:36 +0700

    เพิ่ม Component รอ backend

---
+++ backend/src/controllers/employee.controller.ts
@@ -0,0 +1,19 @@
+interface Employee {
+    id: number;
+    name: string;
+    position: string;
+
+}
+
+let employee : Employee[] = [
+    {
+        id : 1,
+        name : "Punyawee",
+        position : "Dev"
+    },
+    {
+        id : 2,
+        name: "Walailak",
+        position: "Nurse"
+    }
+];
